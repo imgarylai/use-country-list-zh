477bead5cb7e98bc3031c5467fe05240d9456c55
Gary Lai <garylai1990@gmail.com>
2026-06-12 10:42:01 -0700

    chore: migrate demo app from eslint to oxlint

--- demo/next.config.ts
+++ demo/next.config.ts
@@ -4,6 +4,9 @@ import path from 'path';
 const nextConfig: NextConfig = {
   transpilePackages: ['use-country-list-zh', 'antd', '@ant-design/icons'],
   outputFileTracingRoot: path.join(__dirname),
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
